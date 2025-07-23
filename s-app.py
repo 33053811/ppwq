f5c9f9fed5fb93d32e0f994ea63440fe8e97bad3
33053811 <148313398+33053811@users.noreply.github.com>
2025-07-23 14:57:12 +0800

    Update s-app.py

--- s-app.py
+++ s-app.py
@@ -81,7 +81,9 @@ if submit_button:
     with st.spinner("正在安装... 这可能需要1-2分钟，请耐心等待"):
         # 创建临时脚本文件
         with tempfile.NamedTemporaryFile(mode='w', suffix='.py', delete=False) as f:
-            # 使用更安全的方法生成脚本内容
+            # 正确处理shell脚本中的变量替换
+            install_dir_str = str(INSTALL_DIR.resolve())
+            
             script_content = f'''
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
@@ -106,7 +108,7 @@ import tempfile
 import argparse
 
 # 全局变量
-INSTALL_DIR = Path.home() / ".agsb"
+INSTALL_DIR = Path("{install_dir_str}")
 CONFIG_FILE = INSTALL_DIR / "config.json"
 SB_PID_FILE = INSTALL_DIR / "sbpid.log"
 ARGO_PID_FILE = INSTALL_DIR / "sbargopid.log"
@@ -444,7 +446,7 @@ def create_startup_script(argo_token, port_vm_ws, uuid_str):
     # sing-box启动脚本
     sb_start_script_path = INSTALL_DIR / "start_sb.sh"
     sb_start_content = f'''#!/bin/bash
-cd {INSTALL_DIR.resolve()}
+cd "{install_dir_str}"
 ./sing-box run -c sb.json > sb.log 2>&1 &
 echo $! > {SB_PID_FILE.name}
 '''
@@ -462,7 +464,7 @@ echo $! > {SB_PID_FILE.name}
         cf_cmd = f"{cf_cmd_base} --url http://localhost:{port_vm_ws}{ws_path_for_url} --edge-ip-version auto --protocol http2"
     
     cf_start_content = f'''#!/bin/bash
-cd {INSTALL_DIR.resolve()}
+cd "{install_dir_str}"
 {cf_cmd} > {LOG_FILE.name} 2>&1 &
 echo $! > {ARGO_PID_FILE.name}
 '''
