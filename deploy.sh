0fed9d47ce1fc90a0669ffbae8acf16f78f78b02
33053811 <148313398+33053811@users.noreply.github.com>
2025-08-15 09:41:19 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ if ! command -v docker >/dev/null 2>&1; then
     echo "❌ 请先安装 Docker"
     exit 1
 fi
-if ! command -v docker-compose >/dev/null 2>&1; then
-    echo "❌ 请先安装 docker-compose"
+if ! command -v docker compose >/dev/null 2>&1; then
+    echo "❌ 请先安装 docker compose"
     exit 1
 fi
 
 echo "[2/4] 启动服务 ..."
-docker-compose up -d --build
+docker compose up -d --build
 
 echo "[3/4] 等待服务启动 ..."
 sleep 5
